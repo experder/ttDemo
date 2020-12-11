f01a276e586912edcf0ea27959dd2e177d3e3700
contributor <contributor@tethys-framework.de>
2020-12-12 00:11:50 +0100

    add autoloader

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+/*
+ * This file is part of the TT toolbox demo;
+ * Copyright (C) 2014-2021 Fabian Perder (t2@qnote.de) and contributors
+ * TT comes with ABSOLUTELY NO WARRANTY. This is free software, and you are welcome to redistribute it under
+ * certain conditions. See the GNU General Public License (file 'LICENSE' in the root directory) for more details.
+ */
+
+namespace ttdemo;
+
+require_once __DIR__.'/TToolbox/init_web.php';
+
+echo "Hello world! ".time();
