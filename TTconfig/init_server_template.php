3404ba4e6098d6ba56e4b42668efc79ae30c5bde
contributor <contributor@tethys-framework.de>
2020-12-12 10:48:46 +0100

    new server-specific config file "init_server"

---
+++ TTconfig/init_server_template.php
@@ -0,0 +1,16 @@
+<?php
+/*
+ * This file is part of the TT toolbox demo;
+ * Copyright (C) 2014-2021 Fabian Perder (t2@qnote.de) and contributors
+ * TT comes with ABSOLUTELY NO WARRANTY. This is free software, and you are welcome to redistribute it under
+ * certain conditions. See the GNU General Public License (file 'LICENSE' in the root directory) for more details.
+ */
+
+/*
+ * Server specific configuration
+ *
+ * Server: SERVERNAME
+ *
+ */
+
+\tt\database\Database::init('localhost', 'mytt', 'root', 'veryverysafe');
