b5aa6e64a6944922b5c761ea55a7c77d86b113ec
contributor <contributor@tethys-framework.de>
2020-12-16 20:55:48 +0100

    Moved files services

--- TTconfig/init_server_template.php
+++ TTconfig/init_server_template.php
@@ -10,6 +10,7 @@
 
 define('HTTP_RUN', '/TToolbox/run');
 define('HTTP_SKIN', '/TTconfig/skins/skin1');
+\tt\config\Config::setPlatform(\tt\config\Config::PLATFORM_UNKNOWN);
 
 /*
 \tt\config\Config::$DEVMODE = false;
