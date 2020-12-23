ae2ff3dee56981a07e1217019692445f975a2858
contributor <contributor@tethys-framework.de>
2020-12-23 22:17:55 +0100

    Config: Cleaned up defaults

--- TTconfig/init_web.php
+++ TTconfig/init_web.php
@@ -1,10 +1,10 @@
 <?php
 
-require_once dirname(__DIR__).'/TToolbox/core/Config.php';
+require_once dirname(__DIR__).'/TToolbox'.'/core/Config.php';
 \tt\core\Config::set(\tt\core\Config::PROJ_NAMESPACE_ROOT, 'ttdemo');
+\tt\core\Config::set(\tt\core\Config::CFG_DIR, __DIR__);
 /*
 \tt\core\Config::set(\tt\core\Config::CFG_PROJECT_DIR, dirname(__DIR__));
-\tt\core\Config::set(\tt\core\Config::CFG_DIR, __DIR__);
 \tt\core\Config::set(\tt\core\Config::CFG_SERVER_INIT_FILE, __DIR__.'/init_server.php');
 \tt\core\Config::set(\tt\core\Config::DIR_3RDPARTY, dirname(__DIR__).'/thirdparty');
 /**/
