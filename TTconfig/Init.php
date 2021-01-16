96051ba8a32ecf97c9d36df8cca119ef5cf9f379
contributor <contributor@tethys-framework.de>
2021-01-16 13:54:04 +0100

    Config defaults reorganised

--- TTconfig/Init.php
+++ TTconfig/Init.php
@@ -11,13 +11,17 @@ class Init {
 	public static function loadConfig() {
 
 		Config::set(Config::PROJ_NAMESPACE_ROOT, 'ttdemo');
+
 		Config::set(Config::CFG_DIR, __DIR__);
-		/*
+
+		#Config::set(Config::CFG_PROJECT_DIR, '#CFG_PROJECT_DIR');
 		Config::set(Config::CFG_PROJECT_DIR, dirname(__DIR__));
+
 		Config::set(Config::CFG_SERVER_INIT_FILE, __DIR__.'/init_server.php');
+
 		Config::set(Config::DIR_3RDPARTY, dirname(__DIR__).'/thirdparty');
-		Config::set(Config::CFG_API_DIR, Config::get(Config::CFG_DIR) . '/api');
-		/**/
+
+		#Config::set(Config::CFG_API_DIR, Config::get(Config::CFG_DIR) . '/api');
 
 		//Enable multi Autoloader (Autoloader doesn't terminate on error):
 		#require_once dirname(__DIR__) . '/TToolbox'.'/core/Autoloader.php';
