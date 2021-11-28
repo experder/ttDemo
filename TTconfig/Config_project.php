891766b3df11afa4fa1414fb3a1716e41902a1b8
contributor <contributor@tethys-framework.de>
2021-11-28 17:37:18 +0100

    new navigation

--- TTconfig/Config_project.php
+++ TTconfig/Config_project.php
@@ -1,7 +1,7 @@
 <?php
 /*
  * This file is part of the TT toolbox demo;
- * Copyright (C) 2014-2021 Fabian Perder (t2@qnote.de) and contributors
+ * Copyright (C) 2014-2022 Fabian Perder (t2@qnote.de) and contributors
  * TT comes with ABSOLUTELY NO WARRANTY. This is free software, and you are welcome to redistribute it under
  * certain conditions. See the GNU General Public License (file 'LICENSE' in the root directory) for more details.
  */
@@ -9,12 +9,13 @@
 namespace ttcfg;
 
 use tt\core\Config;
+use tt\core\Config_project_interface;
+use tt\core\Modules;
 
-require_once dirname(__DIR__) . '/TToolbox' . '/core/Config.php';
+require_once dirname(__DIR__) . '/TToolbox/core/Config.php';
+require_once dirname(__DIR__) . '/TToolbox/core/Config_project_interface.php';
 
-Config_project::loadConfig();
-
-class Config_project {
+class Config_project implements Config_project_interface {
 
 	public static function loadConfig() {
 
@@ -30,11 +31,11 @@ class Config_project {
 
 		Config::set(Config::DIR_3RDPARTY, dirname(__DIR__) . '/thirdparty');
 
-		Config::set(Config::CFG_API_DIR, Config::get(Config::CFG_DIR) . '/api');
-
 		//Enable multi Autoloader (Autoloader doesn't terminate on error):
 		#require_once dirname(__DIR__) . '/TToolbox'.'/core/Autoloader.php';
 		#\tt\core\Autoloader::multipleAutoloader();
+
+		Config::set(Config::USE_NEW_NAVIGATION, true);
 
 		Config::$project_initialized = true;
 
@@ -56,4 +57,9 @@ class Config_project {
 		Config::init_cli();
 	}
 
+	public static function registerModules(Modules $modules) {
+		$modules->register(new \ttdemo\demo\Module());
+	}
 }
+
+Config_project::loadConfig();
