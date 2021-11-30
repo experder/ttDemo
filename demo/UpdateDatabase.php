6f8ce14370854828caed899de642fd3886a03182
contributor <contributor@tethys-framework.de>
2021-11-30 22:46:55 +0100

    new navigation

--- demo/UpdateDatabase.php
+++ demo/UpdateDatabase.php
@@ -10,6 +10,7 @@ namespace ttdemo\demo;
 
 use tt\core\Config;
 use tt\coremodule\dbmodell\core_pages;
+use tt\coremodule\dbmodell\core_routes;
 use tt\coremodule\pages\Start;
 use ttdemo\demo\demoajax\DemoAjax;
 use ttdemo\demo\democss\DemoCss;
@@ -21,6 +22,30 @@ class UpdateDatabase extends \tt\moduleapi\UpdateDatabase {
 		$this->q(1, core_pages::toSql_insert(DemoAjax::PAGEID, core_pages::TYPE_web, "Ajax demo", DemoAjax::getClass(), null, 101));
 		$this->q(2, core_pages::toSql_insert(DemoCss::PAGEID, core_pages::TYPE_web, "CSS demo", DemoCss::getClass(), null, 102));
 		$this->q(3, core_pages::toSql_insert(Start::PAGEID, core_pages::TYPE_int, "Start", Config::get(Config::HTTP_ROOT) . '/index.php', null, -1));
+		$this->q(4, (new core_routes(array(
+			"route_id"=>DemoAjax::PAGEID,
+			"title"=>"Ajax demo",
+			"parent"=>null,
+			"visible"=>1,
+			"target"=>DemoAjax::getClass(),
+			"orderby"=>101,
+		)))->sql_insert());
+		$this->q(5, (new core_routes(array(
+			"route_id"=>DemoCss::PAGEID,
+			"title"=>"CSS demo",
+			"parent"=>null,
+			"visible"=>1,
+			"target"=>DemoCss::getClass(),
+			"orderby"=>102,
+		)))->sql_insert());
+		$this->q(6, (new core_routes(array(
+			"route_id"=>Start::PAGEID,
+			"title"=>"Start",
+			"parent"=>null,
+			"visible"=>1,
+			"target"=>Config::get(Config::HTTP_ROOT) . '/index.php',
+			"orderby"=>-1,
+		)))->sql_insert());
 
 	}
 }
